8fe1bf6deccd4a1991dde1311c33180da83d820b
SuJeong68 <tidh1640@naver.com>
2023-01-13 15:14:01 +0900

    Feat: 상품 등록 UI 검증제외 구현 & 파일 로컬 업로드

---
+++ src/main/java/shop/yesaladin/front/product/service/impl/CommandProductServiceImpl.java
@@ -0,0 +1,9 @@
+package shop.yesaladin.front.product.service.impl;
+
+import org.springframework.stereotype.Service;
+import shop.yesaladin.front.product.service.inter.CommandProductService;
+
+@Service
+public class CommandProductServiceImpl implements CommandProductService {
+
+}
