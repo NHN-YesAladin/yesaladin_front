f89a46fa6c315ed5444e6f926a2b4575b8dc1005
SuJeong68 <tidh1640@naver.com>
2023-01-13 19:42:37 +0900

    Feat: Object Storage 파일 업로드 구현

--- src/main/java/shop/yesaladin/front/file/controller/FileWebController.java
+++ src/main/java/shop/yesaladin/front/file/controller/FileWebController.java
@@ -4,6 +4,7 @@ import static java.util.UUID.randomUUID;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,9 +13,12 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import shop.yesaladin.front.file.dto.FormDataDto;
+import shop.yesaladin.front.file.service.inter.FileStorageService;
+import shop.yesaladin.front.file.service.inter.StorageAuthService;
 
 /**
  * 파일 관련 페이지를 위한 Controller 입니다.
@@ -35,23 +41,24 @@ import shop.yesaladin.front.file.dto.FormDataDto;
 @RequestMapping("/web/files")
 public class FileWebController {
 
-    private final String FILE_PATH = "/Users/sudang/works/file/";
+    private final FileStorageService fileStorageService;
+    private final StorageAuthService storageAuthService;
 
-    @PostMapping("/file-upload")
-    public ResponseEntity fileUpload(MultipartHttpServletRequest request) throws IOException {
+    @PostMapping("/file-upload/{domainName}/{typeName}")
+    public ResponseEntity fileUpload(
+            MultipartHttpServletRequest request,
+            @PathVariable String domainName,
+            @PathVariable String typeName
+    ) {
         try {
             MultipartFile file = request.getFile("data");
+            String filename = randomUUID() + "." + FilenameUtils.getExtension(file.getOriginalFilename());
 
-            File uploadDir = new File(FILE_PATH);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdirs();
-            }
+            String token = storageAuthService.getAuthToken();
 
-            String filename = randomUUID().toString() + "_" + file.getOriginalFilename();
+            String url = fileStorageService.fileUpload(token, domainName + "/" + typeName, filename, file.getInputStream());
 
-            file.transferTo(Paths.get(FILE_PATH + filename));
-
-            return ResponseEntity.ok().body(Map.of("filename", filename));
+            return ResponseEntity.ok().body(Map.of("url", url));
         } catch (Exception e) {
             // TODO: 에러 보내는거 무조건 다시 만들기!! 이건 임시!!
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
