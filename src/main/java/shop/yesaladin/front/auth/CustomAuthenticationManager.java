a09d35e6e80a7d3ebf30da11449fc0335f747754
alanhakhyeonsong <songs4805@naver.com>
2023-01-17 17:28:45 +0900

    Fix: value property 수정

--- src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
+++ src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
@@ -37,7 +37,7 @@ public class CustomAuthenticationManager implements AuthenticationManager {
 
     private static final int BEARER_LENGTH = 7;
 
-    @Value("${gatewayUrl}")
+    @Value("${yesaladin.gateway}")
     private String gatewayUrl;
 
     private final RestTemplate restTemplate;
