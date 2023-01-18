d716bb79afd3837401dadc39e487e786e36e4deb
alanhakhyeonsong <songs4805@naver.com>
2023-01-18 20:47:08 +0900

    Fix: url 수정

--- src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
+++ src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
@@ -65,7 +65,7 @@ public class CustomAuthenticationManager implements AuthenticationManager {
 
         // TODO: gatewayUrl이 현재 shop 기준으로 되어 있어 실제 gateway + auth url로 변경해야 함.
         ResponseEntity<Void> exchange = restTemplate.exchange(
-                "http://localhost:8081/auth/login",
+                gatewayUrl + "/auth/login",
                 HttpMethod.POST,
                 entity,
                 Void.class
