572e9abcdd3ec8af55badcadbb585dd2de5054ab
SuJeong68 <tidh1640@naver.com>
2023-01-18 23:54:26 +0900

    Feat: 사용자용 전체 조회 구현

--- src/main/java/shop/yesaladin/front/publish/dto/PublisherResponseDto.java
+++ src/main/java/shop/yesaladin/front/publish/dto/PublisherResponseDto.java
@@ -1,22 +1,22 @@
-package shop.yesaladin.front.manager.tag.dto;
+package shop.yesaladin.front.publish.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 태그 조회를 받아와 저장한 dto
+ * 저자 조회를 받아와 저장한 Dto 입니다.
  *
  * @author 이수정
  * @since 1.0
  */
 @Getter
 @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
-public class TagResponseDto {
-    private int id;
+public class PublisherResponseDto {
+
+    private Long id;
     private String name;
 }
