b65d2d7bc81186bd3d783f133da774449e0c623c
SuJeong68 <tidh1640@naver.com>
2023-01-18 23:54:26 +0900

    Feat: 사용자용 전체 조회 구현

--- src/main/java/shop/yesaladin/front/manager/file/service/inter/FileStorageService.java
+++
@@ -1,9 +0,0 @@
-package shop.yesaladin.front.manager.file.service.inter;
-
-import java.io.InputStream;
-import shop.yesaladin.front.manager.file.dto.FileUploadResponseDto;
-
-public interface FileStorageService {
-
-    FileUploadResponseDto fileUpload(String token, String containerName, String fileName, final InputStream inputStream);
-}
