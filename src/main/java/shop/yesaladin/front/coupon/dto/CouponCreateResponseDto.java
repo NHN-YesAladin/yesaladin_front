b175b98e635044f11273a4d969daf6aa515764ce
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-20 13:40:50 +0900

    [Feat] : CouponCreateResponseDto 생성

---
+++ src/main/java/shop/yesaladin/front/coupon/dto/CouponCreateResponseDto.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.front.coupon.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CouponCreateResponseDto {
+
+    private String name;
+    private String couponTypeCode;
+
+}
