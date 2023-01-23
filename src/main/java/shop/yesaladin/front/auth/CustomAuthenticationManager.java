e64f19039b6b324864f29bf80401853ed3c8a3c2
alanhakhyeonsong <songs4805@naver.com>
2023-01-23 20:04:57 +0900

    Feat: login 관련 filter 수정(임시)

--- src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
+++ src/main/java/shop/yesaladin/front/auth/CustomAuthenticationManager.java
@@ -2,10 +2,6 @@ package shop.yesaladin.front.auth;
 
 import static java.util.stream.Collectors.toList;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +21,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.client.RestTemplate;
 import shop.yesaladin.front.member.dto.LoginRequest;
-import shop.yesaladin.front.member.jwt.JwtPayload;
+import shop.yesaladin.front.member.dto.MemberResponse;
 
 /**
  * AuthenticationManager를 custom한 Manager 입니다.
@@ -65,7 +61,6 @@ public class CustomAuthenticationManager implements AuthenticationManager {
         );
         HttpEntity<LoginRequest> entity = new HttpEntity<>(loginRequest, headers);
 
-        // TODO: gatewayUrl이 현재 shop 기준으로 되어 있어 실제 gateway + auth url로 변경해야 함.
         ResponseEntity<Void> exchange = restTemplate.exchange(
                 gatewayUrl + "/auth/login",
                 HttpMethod.POST,
@@ -79,11 +74,28 @@ public class CustomAuthenticationManager implements AuthenticationManager {
             throw new AuthenticationServiceException("");
         }
 
+        if (accessToken.startsWith("Bearer ")) {
+            accessToken = accessToken.substring(7);
+        }
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setBearerAuth(accessToken);
+
+        ResponseEntity<MemberResponse> memberResponse = restTemplate.getForEntity(
+                gatewayUrl + "/shop/v1/members/login/{loginId}",
+                MemberResponse.class,
+                loginRequest.getLoginId(),
+                httpHeaders
+        );
+
+        MemberResponse member = Objects.requireNonNull(memberResponse).getBody();
+        log.info("member={}", member);
+
         // TODO: redis에 토큰 추가
 
         log.info("accessToken={}", accessToken);
 
-        List<SimpleGrantedAuthority> authorities = parseJwt(accessToken.substring(BEARER_LENGTH)).stream()
+        List<SimpleGrantedAuthority> authorities = member.getRoles().stream()
                 .map(SimpleGrantedAuthority::new)
                 .collect(toList());
 
@@ -94,23 +106,5 @@ public class CustomAuthenticationManager implements AuthenticationManager {
                 null,
                 authorities
         );
-    }
-
-    /**
-     * JWT를 parsing 하기 위한 기능입니다.
-     *
-     * @param jwt JWT로 발급된 accessToken 입니다.
-     * @return JWT payload에 저장된 권한 정보를 반환합니다.
-     * @throws JsonProcessingException
-     */
-    private List<String> parseJwt(String jwt) throws JsonProcessingException {
-        String[] jwtSection = jwt.split("\\.");
-        // JWT 는 header, payload, signature 가 "." 으로 연결됨 (header.payload.signature)
-        String jwtPayload = jwtSection[1];
-
-        byte[] decode = Base64.getDecoder().decode(jwtPayload);
-        JwtPayload payload = new ObjectMapper().readValue(new String(decode, StandardCharsets.UTF_8), JwtPayload.class);
-
-        return payload.getRoles();
     }
 }
