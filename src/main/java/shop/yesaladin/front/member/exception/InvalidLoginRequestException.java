3801e08bcd131fa34a4895d5baac21589e5c8b75
alanhakhyeonsong <songs4805@naver.com>
2023-01-25 10:29:33 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/front/member/exception/InvalidLoginRequestException.java
+++ src/main/java/shop/yesaladin/front/member/exception/InvalidLoginRequestException.java
@@ -1,5 +1,11 @@
 package shop.yesaladin.front.member.exception;
 
+/**
+ * 로그인 요청 시 잘못된 입력 값일 경우 발생하는 예외 입니다.
+ *
+ * @author : 송학현
+ * @since : 1.0
+ */
 public class InvalidLoginRequestException extends RuntimeException {
 
     private static final String MESSAGE = "INVALID LOGIN ID, PASSWORD";
