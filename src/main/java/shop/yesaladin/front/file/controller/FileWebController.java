3591f4a7ac8d2a74d6621c8ce5738c4fb9276964
SuJeong68 <tidh1640@naver.com>
2023-01-26 21:44:48 +0900

    Fix: Controller -> RestController 변경

--- src/main/java/shop/yesaladin/front/file/controller/FileWebController.java
+++ src/main/java/shop/yesaladin/front/file/controller/FileWebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import shop.yesaladin.front.file.dto.FileUploadResponseDto;
@@ -22,7 +23,7 @@ import java.util.Map;
  * @since 1.0
  */
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/files")
 public class FileWebController {
