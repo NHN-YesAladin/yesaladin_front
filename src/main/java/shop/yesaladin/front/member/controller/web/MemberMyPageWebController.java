75908212ca8c630377118760c342fa96c540ee56
alanhakhyeonsong <songs4805@naver.com>
2023-01-27 13:19:15 +0900

    Feat: 회원 탈퇴 UI 및 API 연결

--- src/main/java/shop/yesaladin/front/member/controller/web/MemberMyPageWebController.java
+++ src/main/java/shop/yesaladin/front/member/controller/web/MemberMyPageWebController.java
@@ -1,8 +1,13 @@
 package shop.yesaladin.front.member.controller.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.yesaladin.front.member.service.inter.CommandMemberService;
 
 /**
  * 마이페이지 관련 회원 Controller입니다.
@@ -10,10 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author 최예린
  * @since 1.0
  */
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/mypage")
 public class MemberMyPageWebController {
 
+    private final CommandMemberService commandMemberService;
 
+    @GetMapping("/withdraw")
+    public String withdraw() {
+        return "mypage/member/member-withdraw";
+    }
+
+    @PostMapping("/withdraw")
+    public String doWithdraw() {
+        String loginId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        log.info("loginId={}", loginId);
+        commandMemberService.withdraw(loginId);
+
+        return "redirect:/main/index";
+    }
 }
