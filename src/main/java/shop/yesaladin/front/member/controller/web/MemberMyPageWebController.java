351308188a55b7f60b6208c5725879a3690fa113
alanhakhyeonsong <songs4805@naver.com>
2023-01-27 14:57:35 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/front/member/controller/web/MemberMyPageWebController.java
+++ src/main/java/shop/yesaladin/front/member/controller/web/MemberMyPageWebController.java
@@ -13,7 +13,7 @@ import shop.yesaladin.front.member.service.inter.CommandMemberService;
 /**
  * 마이페이지 관련 회원 Controller입니다.
  *
- * @author 최예린
+ * @author 최예린, 송학현
  * @since 1.0
  */
 @Slf4j
@@ -24,11 +24,25 @@ public class MemberMyPageWebController {
 
     private final CommandMemberService commandMemberService;
 
+    /**
+     * 회원 탈퇴 페이지를 view로 리턴시켜주기 위한 Get handler 입니다.
+     *
+     * @return 회원 탈퇴 페이지
+     * @author : 송학현
+     * @since : 1.0
+     */
     @GetMapping("/withdraw")
     public String withdraw() {
         return "mypage/member/member-withdraw";
     }
 
+    /**
+     * 회원 탈퇴를 위한 Post handler 입니다.
+     *
+     * @return 리다이렉트 된 메인 페이지
+     * @author : 송학현
+     * @since : 1.0
+     */
     @PostMapping("/withdraw")
     public String doWithdraw() {
         SecurityContext context = SecurityContextHolder.getContext();
