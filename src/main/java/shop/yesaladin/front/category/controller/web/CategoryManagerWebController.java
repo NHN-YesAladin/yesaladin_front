897c2820b6f2a268b55d4ecf3b18408aa547b0f7
배수한 <qotngks152@naver.com>
2023-01-29 02:49:52 +0900

    Feat: 카테고리 순서 변경 화면 구현

--- src/main/java/shop/yesaladin/front/category/controller/web/CategoryManagerWebController.java
+++ src/main/java/shop/yesaladin/front/category/controller/web/CategoryManagerWebController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.yesaladin.front.category.dto.CategoryResponseDto;
@@ -95,6 +96,11 @@ public class CategoryManagerWebController {
         model.addAttribute("last", last);
         model.addAttribute("blockSize", blockSize);
         return "manager/category/manager-categories";
+    }
+
+    @GetMapping("/order")
+    public String changeOrders() {
+        return "manager/category/manager-change-categories-order";
     }
 
 
