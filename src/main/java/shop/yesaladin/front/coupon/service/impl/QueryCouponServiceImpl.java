9b8f34b4723ed8206e90463dff4afc71da972a0e
서민지 <squarearth1@gmail.com>
2023-01-30 21:10:34 +0900

    [Feat]: 쿠폰 조회 기능 작성

---
+++ src/main/java/shop/yesaladin/front/coupon/service/impl/QueryCouponServiceImpl.java
@@ -0,0 +1,42 @@
+package shop.yesaladin.front.coupon.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import shop.yesaladin.front.common.dto.PaginatedResponseDto;
+import shop.yesaladin.front.config.GatewayConfig;
+import shop.yesaladin.front.coupon.dto.CouponSummaryDto;
+import shop.yesaladin.front.coupon.service.inter.QueryCouponService;
+
+@RequiredArgsConstructor
+@Service
+public class QueryCouponServiceImpl implements QueryCouponService {
+
+    private static final ParameterizedTypeReference<PaginatedResponseDto<CouponSummaryDto>> PAGING_COUPON_TYPE = new ParameterizedTypeReference<>() {};
+    private final RestTemplate restTemplate;
+    private final GatewayConfig gatewayConfig;
+
+    @Override
+    public PaginatedResponseDto<CouponSummaryDto> getTriggeredCouponList(Pageable pageable) {
+        UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(
+                        gatewayConfig.getCouponUrl() + "/v1/coupons")
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .build();
+
+        ResponseEntity<PaginatedResponseDto<CouponSummaryDto>> responseEntity = restTemplate.exchange(
+                uriComponents.toUri(),
+                HttpMethod.GET,
+                null,
+                PAGING_COUPON_TYPE
+        );
+
+        return responseEntity.getBody();
+    }
+}
