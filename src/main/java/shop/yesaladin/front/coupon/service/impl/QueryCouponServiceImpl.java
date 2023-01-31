644da58f9421ec54b1088f62e5b8d6bb6a01bcb2
서민지 <squarearth1@gmail.com>
2023-01-31 19:53:32 +0900

    [Feat]: manager-coupon-list-view 수정

--- src/main/java/shop/yesaladin/front/coupon/service/impl/QueryCouponServiceImpl.java
+++ src/main/java/shop/yesaladin/front/coupon/service/impl/QueryCouponServiceImpl.java
@@ -1,6 +1,8 @@
 package shop.yesaladin.front.coupon.service.impl;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import shop.yesaladin.common.dto.ResponseDto;
 import shop.yesaladin.front.common.dto.PaginatedResponseDto;
 import shop.yesaladin.front.config.GatewayConfig;
 import shop.yesaladin.front.coupon.dto.CouponSummaryDto;
@@ -20,11 +23,12 @@ import shop.yesaladin.front.coupon.service.inter.QueryCouponService;
  * @author 서민지
  * @since 1.0
  */
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class QueryCouponServiceImpl implements QueryCouponService {
 
-    private static final ParameterizedTypeReference<PaginatedResponseDto<CouponSummaryDto>> PAGING_COUPON_TYPE = new ParameterizedTypeReference<>() {
+    private static final ParameterizedTypeReference<ResponseDto<PaginatedResponseDto<CouponSummaryDto>>> PAGING_COUPON_TYPE = new ParameterizedTypeReference<>() {
     };
     private final RestTemplate restTemplate;
     private final GatewayConfig gatewayConfig;
@@ -32,17 +36,17 @@ public class QueryCouponServiceImpl implements QueryCouponService {
     @Override
     public PaginatedResponseDto<CouponSummaryDto> getTriggeredCouponList(Pageable pageable) {
         UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(
-                        gatewayConfig.getCouponUrl() + "/v1/coupons")
+                        gatewayConfig.getCouponUrl() + "/v1/coupons?")
                 .queryParam("page", pageable.getPageNumber())
                 .queryParam("size", pageable.getPageSize())
                 .build();
 
-        ResponseEntity<PaginatedResponseDto<CouponSummaryDto>> responseEntity = restTemplate.exchange(uriComponents.toUri(),
+        ResponseEntity<ResponseDto<PaginatedResponseDto<CouponSummaryDto>>> responseEntity = restTemplate.exchange(uriComponents.toUri(),
                 HttpMethod.GET,
                 null,
                 PAGING_COUPON_TYPE
         );
 
-        return responseEntity.getBody();
+        return Objects.requireNonNull(responseEntity.getBody()).getData();
     }
 }
