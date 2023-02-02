b3ff811c16dfcf2e7345c8c18f81dfdac26514ba
alanhakhyeonsong <songs4805@naver.com>
2023-02-02 16:30:41 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/front/common/controller/IndexController.java
+++ src/main/java/shop/yesaladin/front/common/controller/IndexController.java
@@ -5,12 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import shop.yesaladin.front.member.service.inter.QueryMemberService;
 
+/**
+ * 메인 페이지, 마이 페이지, 관리자 페이지를 리턴하기 위한 Controller 클래스 입니다.
+ *
+ * @author : 송학현
+ * @author : 최예린
+ * @since 1.0
+ */
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final QueryMemberService queryMemberService;
 
+    /**
+     * 메인페이지를 반환시켜줍니다.
+     *
+     * @return 메인페이지
+     * @author 송학현
+     * @since 1.0
+     */
     @GetMapping
     public String main() {
         return "main/index";
@@ -28,13 +42,15 @@ public class IndexController {
         return "mypage/index";
     }
 
+    /**
+     * 관리자페이지를 반환시켜줍니다.
+     *
+     * @return 관리자페이지
+     * @author 송학현
+     * @since 1.0
+     */
     @GetMapping("/manager")
     public String manager() {
         return "manager/index";
-    }
-
-    @GetMapping("/member")
-    public String myPage() {
-        return "member";
     }
 }
