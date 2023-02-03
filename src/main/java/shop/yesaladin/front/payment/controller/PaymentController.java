60457e2b9c578271887b9813df872153004da862
배수한 <qotngks152@naver.com>
2023-02-03 19:42:25 +0900

    Feat: 주문 완성에 맞춰 결제 화면 구현중

--- src/main/java/shop/yesaladin/front/payment/controller/PaymentController.java
+++ src/main/java/shop/yesaladin/front/payment/controller/PaymentController.java
@@ -1,11 +1,13 @@
 package shop.yesaladin.front.payment.controller;
 
 import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.yesaladin.front.payment.dto.PaymentCompleteSimpleResponseDto;
@@ -30,23 +32,18 @@ public class PaymentController {
     /**
      * 토스 페이먼츠의 결제 중 결제 승인 시퀀스를 처리하기 위해 successUrl로 지정된 컨트롤러 메서드
      *
-     * @param paymentKey 결제 구분 키
-     * @param orderId    주문 번호
-     * @param amount     결제 총 금액
+//     * @param paymentKey 결제 구분 키
+//     * @param orderId    주문 번호
+//     * @param amount     결제 총 금액
      * @return 주문 완료 화면
      */
     @GetMapping("/success")
     public String success(
-            @RequestParam("paymentKey") String paymentKey,
-            @RequestParam("orderId") String orderId,
-            @RequestParam("amount") Long amount,
+            HttpServletRequest request,
+            @ModelAttribute PaymentRequestDto requestDto,
             Model model
     ) {
-        PaymentRequestDto requestDto = new PaymentRequestDto(
-                paymentKey,
-                orderId,
-                amount
-        );
+        System.out.println("request = " + request.getQueryString());
         //TODO 주문이 이미 생성이 되어 있어야 shop서버와 통신하여 결제 완료처리 가능
         PaymentCompleteSimpleResponseDto responseDto = paymentService.confirm(requestDto);
         System.out.println("responseDto = " + responseDto);
@@ -67,13 +64,20 @@ public class PaymentController {
     public String failPayment(
             @RequestParam(required = false) String message,
             @RequestParam(required = false) String code,
+            @RequestParam(name = "o-num") String orderNumber,
+            @RequestParam(name = "o-name") String orderName,
+            @RequestParam(name = "o-amount") String amount,
             Model model
     ) {
         //TODO 결제 실패시 정책 수립 후 화면 꾸미기
         model.addAttribute("message", message);
         model.addAttribute("code", code);
+        model.addAttribute("orderNumber", orderNumber);
+        model.addAttribute("orderName", orderName);
+        model.addAttribute("amount", amount);
 
         log.info("{} / {}", code, message);
+        log.info("{} / {} / {}", orderNumber, orderName, amount);
         return "main/order/order-fail";
     }
 
