9824ec3f5045995b2cb2de1cafef76bc1458ff96
배수한 <qotngks152@naver.com>
2023-02-05 01:40:48 +0900

    Fix: 결제 취소도 한 컨트롤러로 통합하므로 인해 api경로 변경

--- src/main/java/shop/yesaladin/front/payment/controller/PaymentController.java
+++ src/main/java/shop/yesaladin/front/payment/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package shop.yesaladin.front.payment.controller;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import shop.yesaladin.common.dto.ResponseDto;
 import shop.yesaladin.front.payment.dto.PaymentCompleteSimpleResponseDto;
 import shop.yesaladin.front.payment.dto.PaymentRequestDto;
 import shop.yesaladin.front.payment.service.inter.PaymentService;
@@ -32,30 +31,33 @@ public class PaymentController {
     /**
      * 토스 페이먼츠의 결제 중 결제 승인 시퀀스를 처리하기 위해 successUrl로 지정된 컨트롤러 메서드
      *
-//     * @param paymentKey 결제 구분 키
-//     * @param orderId    주문 번호
-//     * @param amount     결제 총 금액
-     * @return 주문 완료 화면
+     * @param requestDto 결제에 필요한 필수 정보들
+     * @param model
+     * @return 성공시 주문 완성 페이지, 실패시 주문 실패 페이지
      */
-    @GetMapping("/success")
-    public String success(
+    @GetMapping("/order-pay")
+    public String pay(
             @ModelAttribute PaymentRequestDto requestDto,
             Model model
     ) {
-        PaymentCompleteSimpleResponseDto responseDto = paymentService.confirm(requestDto);
-        System.out.println("responseDto = " + responseDto);
+        ResponseDto<PaymentCompleteSimpleResponseDto> confirmResponse = paymentService.confirm(
+                requestDto);
+        PaymentCompleteSimpleResponseDto responseDto = confirmResponse.getData();
+        log.info("confirm : {}", responseDto);
+
         model.addAttribute("response", responseDto);
+
+        if (!confirmResponse.isSuccess()) {
+            return "main/order/order-fail";
+        }
 
         return "main/order/order-complete";
     }
 
+
     /**
-     * 토스 페이먼츠를 통한 결제 실패시, 돌아오는 컨트롤러 메서드
-     *
-     * @param message 결제 실패 내용
-     * @param code HTTP status code
-     * @param model
-     * @return
+     * 임시 구현 : 토스 failUrl의 대처 방법 고안
+     * 2023.02.05 배수한
      */
     @RequestMapping("/fail")
     public String failPayment(
@@ -77,6 +79,5 @@ public class PaymentController {
         log.info("{} / {} / {}", orderNumber, orderName, amount);
         return "main/order/order-fail";
     }
-
 }
 
