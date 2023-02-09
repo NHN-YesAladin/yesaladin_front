75b04857d9db87197b103431022e25296478822d
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-09 09:05:26 +0900

    [Feat] : 등급별 쿠폰 페이지 와이어프레임 완성

--- src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
+++ src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
@@ -1,6 +1,8 @@
 package shop.yesaladin.front.coupon.controller;
 
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CouponEventWebController {
 
     @GetMapping
-    public String getCouponMainPage() {
+    public String getCouponMainPage(Model model) {
+        model.addAttribute("today", LocalDate.now());
         return "/main/coupon/coupon-main-page";
     }
 
