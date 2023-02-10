98beb907ebc1dae69113f3c3f607511e8a5afd69
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-10 09:45:02 +0900

    [Feat] : 등급별 쿠폰 페이지에서 회원 등급 조회 기능 추가

--- src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
+++ src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
@@ -1,18 +1,24 @@
 package shop.yesaladin.front.coupon.controller;
 
 import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.yesaladin.front.member.service.inter.QueryMemberService;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/coupon")
 public class CouponEventWebController {
 
+    private final QueryMemberService queryMemberService;
+
     @GetMapping
     public String getCouponMainPage(Model model) {
         model.addAttribute("today", LocalDate.now());
+        model.addAttribute("memberGrade", queryMemberService.getMemberGrade());
         return "/main/coupon/coupon-main-page";
     }
 
