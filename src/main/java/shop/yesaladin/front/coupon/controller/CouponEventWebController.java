f176d9aee4f229c84af32655923813c71927809c
Hongdae Kim <mongmeo.dev@gmail.com>
2023-02-12 13:37:28 +0900

    [Feat] : 등급별 쿠폰 지급 요청 기능 추가

--- src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
+++ src/main/java/shop/yesaladin/front/coupon/controller/CouponEventWebController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import shop.yesaladin.coupon.code.TriggerTypeCode;
 import shop.yesaladin.front.common.dto.PaginatedResponseDto;
+import shop.yesaladin.front.config.FrontServerMetaConfig;
+import shop.yesaladin.front.config.GatewayConfig;
 import shop.yesaladin.front.coupon.dto.CouponSummaryWithBoundDto;
+import shop.yesaladin.front.coupon.service.inter.CommandMemberCouponService;
 import shop.yesaladin.front.coupon.service.inter.QueryCouponService;
 import shop.yesaladin.front.member.service.inter.QueryMemberService;
 
@@ -20,6 +23,8 @@ public class CouponEventWebController {
 
     private final QueryMemberService queryMemberService;
     private final QueryCouponService queryCouponService;
+    private final FrontServerMetaConfig frontServerMetaConfig;
+    private final GatewayConfig gatewayConfig;
 
     @GetMapping
     public String getCouponMainPage(Model model, Pageable pageable) {
@@ -31,7 +36,10 @@ public class CouponEventWebController {
         );
         model.addAttribute("today", LocalDate.now());
         model.addAttribute("memberGrade", memberGrade);
+        model.addAttribute("gradeCode", gradeCode);
         model.addAttribute("couponList", couponList);
+        model.addAttribute("frontServerUrl", frontServerMetaConfig.getFrontServerUrl());
+        model.addAttribute("shopServerUrl", gatewayConfig.getShopUrl());
         return "/main/coupon/coupon-main-page";
     }
 
